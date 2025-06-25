becba6757f69682452f43ecc26fdc09bee4a95d5
skkaeb <142946459+skkaeb@users.noreply.github.com>
2025-06-25 10:35:52 -0400

    Implemented loadExpenseFile with file that includes item, price, date

--- src/Expense.java
+++ src/Expense.java
@@ -1,7 +1,17 @@
+import java.time.LocalDate;
+
 //adding a comment for testing - 
 public class Expense {
 	String source;
 	double amount;
 	int yearlyfrequency; //1 for 1 time or once a year, 12 for monthly or or 24 for biweekly
 	//should add contructor(s)
+	String type;
+	LocalDate date; 
+	
+	public Expense(String type, double amount, LocalDate date) {
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+    }
 }
