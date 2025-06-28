2c8a4fc0566264dde6c3ba9fff63cac8e791f4bf
skkaeb <142946459+skkaeb@users.noreply.github.com>
2025-06-28 14:59:15 -0400

    Peyton-LoadIncomeFile() - no gui

---
+++ src/ExpenseManager.java
@@ -0,0 +1,180 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+
+public class ExpenseManager implements Expenser{
+	Scanner scnr = new Scanner(System.in);
+	
+	
+	//expenses holds objects of Expense
+	ArrayList <Expense> expenses = new ArrayList<>();
+	
+	//Establishes that wages stores Wage objects
+	ArrayList <Wage> wages = new ArrayList<>();
+	
+	ArrayList <Wage> income = new ArrayList<>();
+	
+	
+	
+		@Override
+		public boolean loadExpenseFile(String filePath) {
+			//Building this code to read a file with three parts on each line:
+			// item, price, date YYYY-MM-DD (With commas required)
+			
+			try{
+			File file = new File(filePath);
+			Scanner scnr = new Scanner(file);
+
+			
+			while(scnr.hasNextLine()) {	
+				String Line = scnr.nextLine();
+				String [] parts = Line.split(",");
+				
+				if(parts.length == 3) {
+					String type = parts[0];
+		            double amount = Double.parseDouble(parts[1]);
+		            LocalDate date = LocalDate.parse(parts[2]);
+
+		            Expense e = new Expense(type, amount, date);
+		            expenses.add(e);
+				}
+			}
+			 scnr.close();
+			  System.out.println("Loaded " + expenses.size() + " expenses!");
+			}
+			catch(FileNotFoundException e){
+				System.out.println("File not found: " + e.getMessage());
+				return false;
+			}
+			
+			
+			return true;
+		}
+
+		@Override
+		public void addExpense(Expense Ex) {
+			//this method works: someone enters in a Expense object, () and it gets stored into the arrayList expenses
+						
+			//storing the wage object in the array
+			expenses.add(Ex);
+			System.out.println(expenses);
+		}
+
+		@Override
+		public void addMonthlyIncome(Wage W) {
+			//this method works: someone enters in a wage object, (job title, month, salary) and it gets stored into the arrayList wages
+			//wages holds objects of Wage 
+			
+			
+			
+			//storing the wage object in the array
+			wages.add(W);
+			System.out.println(wages);
+			
+			
+		}
+
+		@Override
+		public void PrintFullreport() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void PrintExpensereport() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void PrintIncomereport() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void PrintIncomereportbyTpe() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void PrintExpensebyType() {
+			System.out.println("Enter the type:");
+			String search = scnr.nextLine();
+			String find[];
+			//create arraylist to store all matches and then print them out
+			ArrayList<Expense> typeTracker = new ArrayList<Expense>();
+			
+			for(Expense word : expenses) {
+				//System.out.println("Checking: [" + word.getType() + "] vs [" + search + "]");
+				if(word.getType().trim().equalsIgnoreCase(search)) {
+					typeTracker.add(word);					
+				}
+			}
+			System.out.print(typeTracker);
+		}
+
+		@Override
+		public void exportReport(String reportTitle) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Currency convertForeignCurrency(Currency C, double amount) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean loadIncomeFile(String filePath) {
+			//source= job title
+			//amount = income amount
+			//Month
+			//ArrayList <Wage> income = new ArrayList<>();
+
+			//using arraylist wage
+			//stored in an private arraylist in class user
+			try{
+				File file = new File(filePath);
+				Scanner scnr = new Scanner(file);
+
+				
+				while(scnr.hasNextLine()) {	
+					String Line = scnr.nextLine();
+					String [] parts = Line.split(",");
+					
+					if(parts.length == 3) {
+						String source = parts[0];
+			            double amount = Double.parseDouble(parts[1]);
+			            String Month = parts[2];
+
+			            Wage e = new Wage(source, amount, Month);
+			            income.add(e);
+					}
+				}
+				 scnr.close();
+				  System.out.println("Loaded " + income.size() + " expenses!");
+				}
+				catch(FileNotFoundException e){
+					System.out.println("File not found: " + e.getMessage());
+					return false;
+				}
+			return false;
+		}
+
+		@Override
+		public int whenCanIBuy(String itemname, double price) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public void updateMonthlySavings() {
+			// TODO Auto-generated method stub
+			
+		}
+}
---
+++ src/driver.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class driver {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		ExpenseManager test = new ExpenseManager();
+		//testing loadExpenseFile()
+		//System.out.println("Enter file name: ");
+		//String FileName = scnr.nextLine();
+		 
+		//test.loadExpenseFile(FileName); 
+		
+	
+		//testing loadIncomeFile(String filePath)
+		//use this filename: C:\Users\skkae\Documents\Income.txt
+		System.out.println("Enter file name: ");
+		String FileName = scnr.nextLine();
+		test.loadIncomeFile(FileName);
+		
+		
+		
+		//test run addMonthlyIncome()
+		/*System.out.println("What is your job title:");
+		String job = scnr.nextLine();
+		System.out.println("Enter the month:");
+		String Month = scnr.next();
+		System.out.println("Enter your monthly wage:");
+		double monthlyIncome = scnr.nextDouble();
+		
+		//creating a Wage object
+		Wage wage = new Wage(job, monthlyIncome, Month);
+		test.addMonthlyIncome(wage); */
+		
+		
+		
+		//test run addExpense()
+		boolean runCount = true;
+		while(runCount) {
+		System.out.println("What is the Expense type:");
+		String type = scnr.nextLine().trim();
+		
+		//scnr.nextLine(); 
+		System.out.println("Enter the amount:");
+		double amount = scnr.nextDouble();
+		scnr.nextLine(); 
+		
+		System.out.println("Enter a date (YYYY-MM-DD):");
+	    String input = scnr.nextLine().trim();
+	    LocalDate date = LocalDate.parse(input);
+	    
+		//creating a Wage object
+		Expense expense = new Expense(type, amount, date);
+		test.addExpense(expense);
+		
+		System.out.println("Would you like to add another expense? (yes or no)");
+		String answer = scnr.nextLine().trim();
+			    
+		if (answer.equalsIgnoreCase("no")) {
+			runCount = false;
+			break;
+		}
+		}
+		
+		test.PrintExpensebyType();
+		
+		
+	
+	
+	
+	}
+}
