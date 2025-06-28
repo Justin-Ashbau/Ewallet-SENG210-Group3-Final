55816e2178913e23d47223dea9da7a2418f1c375
skkaeb <142946459+skkaeb@users.noreply.github.com>
2025-06-28 15:06:01 -0400

    peyton-LoadIncomeFile()- no gui

--- src/Expense.java
+++ src/Expense.java
@@ -1,7 +1,25 @@
+import java.time.LocalDate;
+
 //adding a comment for testing - 
 public class Expense {
 	String source;
 	double amount;
 	int yearlyfrequency; //1 for 1 time or once a year, 12 for monthly or or 24 for biweekly
-	//should add contructor(s)
+	
+	String type;
+	LocalDate date; 
+	
+	public Expense(String type, double amount, LocalDate date) {
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+    }
+	@Override
+    public String toString() {
+        return   type + " , " +  amount + " , " + date;
+    }
+	public String getType() {
+	    return type;
+	}
+
 }
